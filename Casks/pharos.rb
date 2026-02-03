fd30e9699670cbaf071379b99a8dc4f4efc65a52
NeodymiumPhish <neodymiumphish@gmail.com>
2026-02-03 10:27:46 -0500

    Update pharos.rb

--- Casks/pharos.rb
+++ Casks/pharos.rb
@@ -1,6 +1,6 @@
 cask "pharos" do
   version "1.0.1"
-  sha256 "a261dc3c5d1c2118e9e4019129e583fe277e0731cf7d758900baac445e597489"  # SHA256 of your DMG
+  sha256 "a261dc3c5d1c2118e9e4019129e583fe277e0731cf7d758900baac445e597489"
 
   url "https://github.com/NeodymiumPhish/Pharos/releases/download/v#{version}/Pharos_#{version}_aarch64.dmg"
   name "Pharos"
@@ -8,6 +8,12 @@ cask "pharos" do
   homepage "https://github.com/NeodymiumPhish/Pharos"
 
   app "Pharos.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{appdir}/Pharos.app"],
+                   sudo: false
+  end
 
   zap trash: [
     "~/Library/Application Support/com.pharos.client",
