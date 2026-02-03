37be0da5385e22d84caae1a65cb0c58a1c4fdac4
NeodymiumPhish <neodymiumphish@gmail.com>
2026-02-03 10:22:17 -0500

    Update pharos.rb

--- Casks/pharos.rb
+++ Casks/pharos.rb
@@ -2,9 +2,9 @@ cask "pharos" do
   version "1.0.1"
   sha256 "32d91fd0408e330986c89df7fab18b59418cee246e4dcd0a9a7a8d22d76595d5"  # SHA256 of your DMG
 
-  url "https://github.com/NeodymiumPhish/Pharos/releases/download/v#{version}/Pharos_#{version}_x64.dmg"
+  url "https://github.com/NeodymiumPhish/Pharos/releases/download/v#{version}/Pharos_#{version}_aarch64.dmg"
   name "Pharos"
-  desc "High-performance PostgreSQL client with Liquid Glass UI"
+  desc "High-performance PostgreSQL client with modern UI"
   homepage "https://github.com/NeodymiumPhish/Pharos"
 
   app "Pharos.app"
